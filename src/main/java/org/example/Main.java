d43ed8129204d53cab10ddde7ca587438db6e8a0
jackytank <balisongian@gmail.com>
2023-05-29 00:16:59 +0700

    push solved problems from days before

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,154 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    // https://leetcode.com/problems/contains-duplicate/
+    public static boolean containsDuplicate(int[] nums) {
+        if (nums.length == 1) {
+            return false;
+        }
+        if (nums.length == 2 && nums[0] != nums[1]) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return nums.length != set.size();
+    }
+
+    // https://leetcode.com/problems/missing-number/
+    public static int missingNumber(int[] nums) {
+        int sum = 0;
+        int sumExpect = 0;
+        boolean existNum_0 = false;
+        for (int i = 0; i < nums.length; i++) {
+            sumExpect += i + 1;
+            sum += nums[i];
+            if (nums[i] == 0) existNum_0 = true;
+        }
+        if (!existNum_0) return 0;
+        return sumExpect - sum;
+    }
+
+    // https://leetcode.com/problems/binary-search/
+    public static int binarySearch(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+
+    // https://leetcode.com/problems/length-of-last-word/
+    public static int lengthOfLastWord(String s) {
+        String[] arr = s.split(" ");
+        return arr[arr.length - 1].length();
+    }
+
+    // https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+    public int strStr(String haystack, String needle) {
+        char firstCharNeedle = needle.charAt(0);
+        if (haystack.length() < needle.length()) return -1;
+        for (int i = 0; i < haystack.length(); i++) {
+            int count = 0;
+            if (i + needle.length() > haystack.length()) return -1;
+            if (haystack.charAt(i) == firstCharNeedle) {
+                for (int j = 1; j < needle.length(); j++) {
+                    if (haystack.charAt(i + j) == needle.charAt(j)) {
+                        count++;
+                    }
+                }
+                if (count == needle.length() - 1) return i;
+            }
+        }
+        return -1;
+    }
+
+    // https://leetcode.com/problems/valid-parentheses/
+    public static boolean isValid(String s) {
+        Stack stack = new Stack<Character>();
+        boolean res = false;
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (stack.size() >= 1) {
+                char last = (char) stack.lastElement();
+                if ((cur == last + 1) || (cur == last + 2)) {
+                    res = true;
+                    stack.pop();
+                    continue;
+                } else {
+                    res = false;
+                }
+            }
+            stack.push(cur);
+        }
+        return stack.isEmpty();
+    }
+
+    // https://leetcode.com/problems/roman-to-integer/
+    public int romanToInt(String s) {
+        if (s.length() < 1 || s.length() > 15) return 0;
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'I') {
+                if (i + 1 < s.length() && s.charAt(i + 1) == 'V') {
+                    res += 4;
+                    i++;
+                    continue;
+                }
+                if (i + 1 < s.length() && s.charAt(i + 1) == 'X') {
+                    res += 9;
+                    i++;
+                    continue;
+                }
+                res += 1;
+            }
+            if (s.charAt(i) == 'V') res += 5;
+            if (s.charAt(i) == 'X') {
+                if (i + 1 < s.length() && s.charAt(i + 1) == 'L') {
+                    res += 40;
+                    i++;
+                    continue;
+                }
+                if (i + 1 < s.length() && s.charAt(i + 1) == 'C') {
+                    res += 90;
+                    i++;
+                    continue;
+                }
+                res += 10;
+            }
+            if (s.charAt(i) == 'L') res += 50;
+            if (s.charAt(i) == 'C') {
+                if (i + 1 < s.length() && s.charAt(i + 1) == 'D') {
+                    res += 400;
+                    i++;
+                    continue;
+                }
+                if (i + 1 < s.length() && s.charAt(i + 1) == 'M') {
+                    res += 900;
+                    i++;
+                    continue;
+                }
+                res += 100;
+            }
+            if (s.charAt(i) == 'D') res += 500;
+            if (s.charAt(i) == 'M') res += 1000;
+        }
+        return res;
+    }
+
     // https://leetcode.com/problems/top-k-frequent-elements/
     public static int[] topKFrequent(int[] nums, int k) {
         return null;
